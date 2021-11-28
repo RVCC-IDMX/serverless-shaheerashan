56b466f6aa93e9dd9244d5c48da688373b4f00ea
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-28 16:06:22 -0500

    Main commit

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+/* eslint-disable quotes */
+const getRandomDadJoke = async () => {
+  const url = "https://icanhazdadjoke.com/";
+  const jokeStream = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const jsonJoke = await jokeStream.json();
+  const { joke } = jsonJoke;
+  return joke;
+};
+
+const displayJoke = (joke) => {
+  const h1 = document.querySelector("h1");
+  h1.textContent = joke;
+};
+
+const refreshJoke = async () => {
+  const joke = await getRandomDadJoke();
+  displayJoke(joke);
+};
+
+// load 1st joke
+refreshJoke();
+
+setInterval(refreshJoke, 10000);

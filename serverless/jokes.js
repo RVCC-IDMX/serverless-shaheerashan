57dfb9d825e236741060eadff4ab2943db311138
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-28 16:23:08 -0500

    Add final changes

---
+++ serverless/jokes.js
@@ -0,0 +1,27 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+const axios = require("axios");
+
+/*
+ https://axios-http.com/docs/res_schema
+*/
+
+exports.handler = async (event, context) => {
+  const url = "https://icanhazdadjoke.com/";
+  try {
+    const jokeStream = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "axios 0.21.1",
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jokeStream.data),
+    };
+  } catch (err) {
+    console.log(err);
+    return { statusCode: 422, body: err.stack };
+  }
+};
